b8a2009906770d513dfda511e8966a8293ad62f7
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2023-12-26 15:45:38 +0330

    splitting components practice

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,13 @@
-import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
 function ExpenseItem(props) {
 
-    const expenseDate = new Date(2021, 2, 12);
-    const expenseItem = 'car Insurance';
-    const expenseAmount = 247;
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
+                <div className='expense-item__price'>{props.amount}</div>
             </div>
         </div>
     );
