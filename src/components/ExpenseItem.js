bd36bd62f1531de64bdec1ea8b5d50e37f0951f5
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2024-01-02 12:55:37 +0330

    JSX under the hood practice

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,12 @@
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
+
+
 function ExpenseItem(props) {
 
     return (
+        
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
